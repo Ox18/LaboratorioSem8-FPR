ed9f69aad773b0011f91d502c918a1484166b775
Alex <wilmer.delgadoalama09@gmail.com>
2021-09-10 14:55:01 -0500

    first commit

--- app/src/main/java/com/example/fprappseman8/Main2Activity.java
+++ app/src/main/java/com/example/fprappseman8/Main2Activity.java
@@ -7,12 +7,16 @@ import android.webkit.WebView;
 
 public class Main2Activity extends AppCompatActivity {
 
+    private String url;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
+        url = getIntent().getExtras().getString("url");
+
         WebView myWebView = (WebView) findViewById(R.id.webView);
-        myWebView.loadUrl("https://www.google.com/");
+        myWebView.loadUrl(url);
     }
 }
--- app/src/main/java/com/example/fprappseman8/MainActivity.java
+++ app/src/main/java/com/example/fprappseman8/MainActivity.java
@@ -6,17 +6,23 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText searchText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        searchText = (EditText)findViewById(R.id.searchText);
     }
 
     public void Ver(View view){
         Intent i = new Intent(this, Main2Activity.class);
+        i.putExtra("url", searchText.getText().toString());
         startActivity(i);
     }
 }
